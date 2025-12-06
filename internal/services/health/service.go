2b169641c6e3377c333efcf8d41ce99da6c4aa44
Maksym Trofimenko <maksym@container-registry.com>
2025-12-06 18:41:47 +0000

    update hga

--- internal/services/health/service.go
+++ internal/services/health/service.go
@@ -9,13 +9,13 @@ type Checker struct {
 	grpc_health_v1.UnimplementedHealthServer
 }
 
-func (s *Checker) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
+func (s *Checker) Check(_ context.Context, _ *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
 	return &grpc_health_v1.HealthCheckResponse{
 		Status: grpc_health_v1.HealthCheckResponse_SERVING,
 	}, nil
 }
 
-func (s *Checker) Watch(req *grpc_health_v1.HealthCheckRequest, server grpc_health_v1.Health_WatchServer) error {
+func (s *Checker) Watch(_ *grpc_health_v1.HealthCheckRequest, server grpc_health_v1.Health_WatchServer) error {
 	return server.Send(&grpc_health_v1.HealthCheckResponse{
 		Status: grpc_health_v1.HealthCheckResponse_SERVING,
 	})
